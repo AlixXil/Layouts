6a41c0837c349e37dbec230e91ee095836ce8139
AlixXil <kuzmin1989@outlook.com>
2020-08-22 12:46:55 +0300

    complite photo albums

---
+++ PhotoAlbums/build/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+	document.querySelector(".tab-menu-btn").addEventListener('click', ()=>{
+		document.querySelector(".menu").classList.toggle("active")
+	})
+})
