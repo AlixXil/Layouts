2b7b6f46485f8d1770565fe834f0099a04cd262b
AlixXil <kuzmin1989@outlook.com>
2020-09-29 00:41:09 +0300

    adaptive

--- MusicShop/src/js/script.js
+++ MusicShop/src/js/script.js
@@ -11,5 +11,72 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 	$('.item-slider').slick({
 		slidesToShow: 4,
+		responsive: [{
+			breakpoint: 1120,
+			settings: {
+				arrows: false
+			}
+		}, {
+			breakpoint: 780,
+			settings: {
+				slidesToShow: 3,
+				arrows: false
+			}
+		}, {
+			breakpoint: 570,
+			settings: {
+				slidesToShow: 2,
+				arrows: false
+			}
+		}, {
+			breakpoint: 450,
+			settings: {
+				slidesToShow: 1,
+				arrows: false
+			}
+		}]
 	});
+
+	$('.actions__list').slick({
+		slidesToShow: 2,
+		responsive: [{
+			breakpoint: 1070,
+			settings: {
+				arrows: false
+			}
+		}, {
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 1,
+				arrows: false
+			}
+		}]
+	})
+	$('.productions__list').slick({
+		slidesToShow: 6,
+		arrows: false,
+		autoplay: true,
+		delay: 100,
+		responsive: [{
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 5
+			}
+		}, {
+			breakpoint: 620,
+			settings: {
+				slidesToShow: 4
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 3
+			}
+		}, {
+			breakpoint: 350,
+			settings: {
+				slidesToShow: 2
+			}
+		}]
+	})
 })
