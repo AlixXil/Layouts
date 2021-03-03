160ab63a3ba61682cf80637e1dec3b9dbebeb28f
Алексей Кузьмин <kuzmin1989@outlook.com>
2021-03-03 20:28:02 +0300

    update

--- Ontario/gulpfile.js
+++ Ontario/gulpfile.js
@@ -1,153 +1,184 @@
-//настрока для обновления браузера
-const browserSyncConfig = {
-	server: {
-		baseDir: './build'
-	},
-	host: 'localhost',
-	open: false,
-	port: 3000,
-	logPrefix: "server"
-};
-// пути к фалам
-const path = {
-	build: {
-		html: 'build/',
-		js: 'build/js/',
-		css: 'build/css/',
-		img: 'build/img/',
-		fonts: 'build/fonts/',
-		libs: 'build/libs/'
-	},
-	src: {
-		html: 'src/*.html',
-		pug: 'src/*.pug',
-		js: [
-			'src/js/main.js', 		//сперва берем main.js 
-			'src/js/*.js'					//далее все остальные по алфавиту
-		],
-		scss: 'src/scss/*.scss',	// sttyle.scss - главный файл стилей
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*',
-		libs: 'src/libs/**/*.*'
-	},
-	watch: {
-		html: 'src/*.html',
-		pug: ['src/*.pug', 'src/_partianHtml/*.pug', 'src/layout/*.pug'],
-		js: 'src/js/*.js',
-		// sass: 'src/sass/*.sass',
-		scss: 'src/scss/*.scss',
-		// css: 'src/css/*.css',
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/**/*.*'
-	},
-	clean: './build/*'
-};
+let buildFolder = "build"
+let sourceFolder = "src"
 
-// модули
-const { src, dest, watch, parallel, series } = require('gulp'),
-	pug = require('gulp-pug'),
-	sass = require('gulp-sass'),
-	browser_sync = require('browser-sync').create(),
-	concat = require('gulp-concat'),
-	del = require('del'),
-	cleanCss = require('gulp-clean-css'),
-	prefix = require('gulp-autoprefixer'),
-	browsersList = require('browserslist'),
-	rigger = require('gulp-rigger');
-
-// задачи
-
-// browser-sync
-function bs() {
-	browser_sync.init(browserSyncConfig);
+//пути до файлов
+let path = {
+  build: { // сборка проекта
+    html: "./" + buildFolder + "/",
+    css: "./" + buildFolder + "/css/",
+    js: "./" + buildFolder + "/js/",
+    img: "./" + buildFolder + "/img/",
+    fonts: "./" + buildFolder + "/fonts/",
+  },
+  src: { //исходники
+    html: "./" + sourceFolder + "/*.html",
+    scss: "./" + sourceFolder + "/scss/style.scss",
+    js: "./" + sourceFolder + "/js/script.js",
+    img: "./" + sourceFolder + "/img/**/*.+(jpg|png|svg|webp|ico)",
+    imgToWebp: "./" + sourceFolder + "/img/**/*.+(jpg|png|jpeg)",
+    fonts: "./" + sourceFolder + "/fonts/",
+  },
+  watch: { // отслеживание файлов
+    html: "./" + sourceFolder + "/*.html",
+    scss: "./" + sourceFolder + "/scss/*.scss",
+    js: "./" + sourceFolder + "/js/*.js",
+    img: "./" + sourceFolder + "/img/*",
+  },
+  clean: "./" + buildFolder + "/"
 }
 
-// собираем pug файлы
-function html_pug() {
-	return src(path.src.pug)
-	.pipe(pug())
-	.pipe(dest(path.build.html))
-	.pipe(browser_sync.stream());
+// плагины
+let { src, dest, watch, series, parallel } = require('gulp'),
+  del = require('del'),
+  fileinclude = require('gulp-file-include'),
+  browsersync = require('browser-sync').create(),
+  scss = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  group_media = require('gulp-group-css-media-queries'),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify-es').default,
+  imagemin = require('gulp-imagemin'),
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  fonter = require('gulp-fonter'),
+  fs = require('fs'),
+  webp = require('gulp-webp')
+
+// обновление браузера
+function browserSync() {
+  browsersync.init({
+    server: {
+      baseDir: "./" + buildFolder + "/"
+    },
+    port: 3000,
+    notify: false,
+    open: false
+  })
 }
 
-// собираем html файлы 
+//обработка html файлов
 function html() {
-	return src(path.src.html)
-	.pipe(dest(path.build.html));
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
 }
 
-// собираем scss файлы
-function scssToCss() {
-	return src(path.src.scss)
-	.pipe(sass())
-	.pipe(prefix({
-		browsersList,
-		cascade: false
-	}))
-	.pipe(cleanCss({compatibility: 'ie8'}))
-	.pipe(dest(path.build.css))
-	.pipe(browser_sync.stream());
-}
-// собираем scss файлы dev
-function scssToCss_dev() {
-	return src(path.src.scss)
-	.pipe(sass())
-	.pipe(dest(path.build.css))
-	.pipe(browser_sync.stream());
+//обработка scss файлов
+function css() {
+  return src(path.src.scss)
+    .pipe(scss({ outputStyle: "expanded" }))
+    .pipe(group_media())
+    .pipe(autoprefixer({
+      overrideBrowserslist: ["last 5 versions"],
+      cascade: true
+    }))
+    .pipe(dest(path.build.css))
+    .pipe(cleanCss())
+    .pipe(rename({ extname: ".min.css" }))
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
 }
 
-// собираем js файлы
+//обработка js файлов
 function js() {
-	return src(path.src.js)
-	.pipe(rigger())
-	.pipe(concat('script.js'))
-	.pipe(dest(path.build.js))
-	.pipe(browser_sync.stream());
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
 }
-// собираем js файлы dev
-function js_dev() {
-	return src(path.src.js)
-	.pipe(rigger())
-	.pipe(concat('script.js'))
-	.pipe(dest(path.build.js))
-	.pipe(browser_sync.stream());
-}
-// собираем шрифты
-function fonts() {
-	return src(path.src.fonts)
-	.pipe(dest(path.build.fonts));
-}
-// собираем картинки
+
+//обработка картинок
 function images() {
-	return src(path.src.img)
-	.pipe(dest(path.build.img));
-}
-// собираем библиотеки
-function libs() {
-	return src(path.src.libs)
-	.pipe(dest(path.build.libs));
+  return src(path.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      interlaced: true,
+      optimizationLevel: 3
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
 }
 
-//watch
-function watchFiles() {
-	bs();
-	
-	watch(path.watch.html, html);
-	watch(path.watch.pug, html_pug);
-	watch(path.watch.scss, scssToCss_dev);
-	watch(path.watch.js, js_dev);
-	watch(path.watch.img, images);
-
-	watch(path.watch.html).on('change', browser_sync.reload);
+function imagesToWebp() {
+  return src(path.src.imgToWebp)
+    .pipe(webp())
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      interlaced: true,
+      optimizationLevel: 3
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
 }
-// очистка проекта
+
+function fontsToBuild() {
+  return src([path.src.fonts + '*.woff', path.src.fonts + '*.woff2'])
+    .pipe(dest(path.build.fonts))
+}
+
+// очистка папки сборки
 function clean() {
-	return del(path.clean);
+  return del(path.clean)
+}
+
+function watchFiles() {
+  watch([path.watch.html], html)
+  watch([path.watch.scss], css)
+  watch([path.watch.js], js)
+  watch([path.watch.img], images)
 }
 
 
-// экпорт 
-exports.clean = clean;
-exports.build = series(clean, parallel(scssToCss, html, html_pug, js, fonts, images, libs));
-exports.build_dev = series(clean, parallel(scssToCss_dev, html, html_pug, js_dev, fonts, images, libs));
-exports.default = series(exports.build_dev, watchFiles);
+// вспомагательные функции
+// конвертация шрифтов
+function otf2ttf() {
+  return src([sourceFolder + '/fonts/*.otf'])
+    .pipe(fonter({ formats: ['ttf'] }))
+    .pipe(dest(sourceFolder + '/fonts/'))
+}
+
+//обработка шрифтов
+function fonts() {
+  src(path.src.fonts + '*.ttf')
+    .pipe(ttf2woff())
+    .pipe(dest(path.src.fonts))
+  return src(path.src.fonts + '*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest(path.src.fonts))
+}
+
+// добавление шрифтов в стили
+function fontStyle() {
+  let file_content = fs.readFileSync(sourceFolder + '/scss/fonts.scss')
+  if (file_content == '') {
+    fs.writeFile(sourceFolder + '/scss/fonts.scss', '', cb)
+    return fs.readdir(path.build.fonts, function(err, items) {
+      if (items) {
+        let c_fontname
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.')
+          fontname = fontname[0]
+          if (c_fontname != fontname) {
+            fs.appendFile(sourceFolder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb)
+          }
+          c_fontname = fontname
+        }
+      }
+    })
+  }
+}
+
+function cb() {}
+
+exports.otf2ttf = otf2ttf
+  // подготовка шрифтов
+exports.fonts = series(fonts, fontStyle)
+const build = series(clean, parallel(html, css, js, imagesToWebp, images, fontsToBuild))
+exports.build = build
+exports.default = series(clean, build, parallel(browserSync, watchFiles))
